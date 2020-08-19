11ddda8b279c8ff3286acc9e89863cd8105f1454
GiangBeo <giangbeoit@gmail.com>
2020-08-19 13:43:31 +0700

    Init project

---
+++ src/ZipFacade.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Webike\Zip;
+
+use Illuminate\Support\Facades\Facade;
+
+class ZipFacade extends Facade
+{
+
+    protected static function getFacadeAccessor()
+    {
+        return 'zip';
+    }
+}
